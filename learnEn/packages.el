4f3fd5d44c087146137cedd27fb7c0b1c77583f2
周子豪 <1042181618@qq.com>
2016-12-07 23:27:33 +0800

    add a base keybing

--- learnEn/packages.el
+++ learnEn/packages.el
@@ -25,38 +25,19 @@
 
 ;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
 ;;   define the functions `learnEn/pre-init-PACKAGE' and/or
-;;   `learnEn/post-init-PACKAGE' to customize the package as it is loaded.
+;;   `learnEn/pst-init-PACKAGE' to cusomize the package as it is loaded.
 
 ;;; Code:
 
 (defconst learnEn-packages
-  '()
-  "The list of Lisp packages required by the learnEn layer.
+  '(youdao-dictionary)
+  )
 
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
-
+(defun learnEn/init-youdao-dictionary()
+  (use-package youdao-dictionary
+    :init
+    (spacemacs/set-leader-keys "os" 'youdao-dictionary-search-at-point+)
+    )
+  )
 
 ;;; packages.el ends here
